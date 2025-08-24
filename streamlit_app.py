55cb576cf65c74b25ae4025244b2cdcb9bd4153f
yolandayxf <123848710+yolandayxf@users.noreply.github.com>
2025-08-23 22:01:03 -0400

    Update streamlit_app.py
    put the data from API at the bottom of the UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,6 @@ from snowflake.snowpark.functions import col
 import requests
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -64,4 +59,9 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
+
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
