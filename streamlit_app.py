573a15bde7057039018df592a2d913ea7380c172
yolandayxf <123848710+yolandayxf@users.noreply.github.com>
2025-08-23 22:04:47 -0400

    Update streamlit_app.py
    only show nutrition info for selected ingredients

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -60,8 +62,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
 
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
